ac8fdcdd5f1d88ff53b818079b31b04af92e80a5
Andrii Chychkan <mywork1728@gmail>
2025-10-25 20:19:56 +0300

    added full classes Car and Carwash

--- app/main.py
+++ app/main.py
@@ -1,8 +1,42 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = round(average_rating, 1)
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        total_cost = 0
+        for car in cars:
+            print(car.clean_mark)
+            if car.clean_mark < self.clean_power:
+                total_cost += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return total_cost
+
+    def calculate_washing_price(self, car: Car) -> float:
+        sum_of_washing = ((car.comfort_class
+                          * (self.clean_power - car.clean_mark)
+                          * self.average_rating)
+                          / self.distance_from_city_center)
+        return round(sum_of_washing, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, add_rating: float) -> None:
+        new_rating = (((self.average_rating
+                       * self.count_of_ratings) + add_rating)
+                      / (self.count_of_ratings + 1))
+        self.count_of_ratings += 1
+        self.average_rating = round(new_rating, 1)
